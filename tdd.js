e012e44330fc41640a4c8b5f095a15c78f7670e5
Jennifer-Raymondo <jenniferraymondo295@gmail.com>
2025-11-12 10:21:30 +0300

    changes made

---
+++ tdd.js
@@ -0,0 +1,7 @@
+// TDD in JavaScript
+
+// We’ll use a simple scenario: writing a function that returns "Fizz", "Buzz", or "FizzBuzz" based on a number.
+
+// We’ll assume we’re using Jest, a common testing library for JavaScript.
+
+
